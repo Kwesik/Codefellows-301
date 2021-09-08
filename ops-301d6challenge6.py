92d606be69e28b32db61c62b2e94ca623319a9ee
Kwesi Keller <kkeller85@gmail.com>
2021-09-07 23:40:52 -0700

    variable script

---
+++ ops-301d6challenge6.py
@@ -0,0 +1,17 @@
+# Script:   PythonHw
+#Author:    Kwesik
+#Last rev: 20210907
+#Purpose:   Print (name, Age, has_W2)
+
+#!/bin/bash
+
+#Variables
+name = "bob"
+Age = 54
+has_W2 = True
+
+#Main
+print (name, Age, has_W2)
+
+
+#End
