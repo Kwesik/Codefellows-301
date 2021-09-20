c7ffc0a9c1b0b86589ed5951e9b551a6f295ede9
Kwesi Keller <kkeller85@gmail.com>
2021-09-19 20:00:42 -0700

    Directory creation

--- ops-301d7challenge7.py
+++ ops-301d7challenge7.py
@@ -5,16 +5,16 @@
 #Purpose:    
 
 
-#Libraries
 import os
 
-#Variables
-path=input("Enter Filepath:")
+# Read user input here into a variable
+var1 =input("Please enter a file path: ")
 
-
-#Functions
-
-
-#Main
+# Prints from bottom up all dir, sub dir, and files
+for root, dirs, files in os.walk(".", topdown=False):
+   for name in files:
+      print(os.path.join(root, name))
+   for name in dirs:
+      print(os.path.join(root, name))
 
 #End
